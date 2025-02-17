24e76c1a745c77d8c571fe9b04dea670e690f9bd
GarrettBrunsch <brunscgv1595@student.laccd.edu>
2025-02-16 17:26:27 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Garrett Brunsch
+// Lab #0
+// This lab is a revision to the course module's Lab #0. The goal of the lab was to add personal information, fix syntax errors, and correct logical errors
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+        cout << "\nMy name is "
+        "Garrett Vincent Brunsch" << endl;
+
+    cout << "\nEnter the first number: ";
+    cin >> num1;
+
+    cout << "\nEnter the second number: ";
+    cin >> num2;
+
+    sum = num1 + num2;
+    cout << "\nThe sum of " << num1 << " and " << num2 << " is " << sum << endl;
+
+    return 0;
+}
+
+/*
+My name is Garrett Vincent Brunsch
+
+Enter the first number: 5
+
+Enter the second number: 4
+
+The sum of 5 and 4 is 9
+*/
+
